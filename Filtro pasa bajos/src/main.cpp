94407c846442831efa68dfd4e0f3d5147f3aa9a1
Willy <wilsonoh@outlook.es>
2019-06-18 14:35:24 -0400

    Modify platformio.ini, no funcional code

--- Filtro pasa bajos/src/main.cpp
+++ Filtro pasa bajos/src/main.cpp
@@ -1,9 +1,24 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+//Filtro Media Móvil como Pasa Bajos
+//An=a*M+(1-a)*An
+//alpha 1: Sin filtro
+//alpha 0: Filtrado totalmente
+//alpha clásico 0.05
+
+int adc_filtrado = 0;
+int adc_raw = 0;
+#define alpha 0.05
+
+void setup(){
+   Serial3.begin(4800);
+   Serial3.print("Holaaa"); 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop(){
+  /*  adc_raw = analogRead(PA0);
+   adc_filtrado = (alpha*adc_raw) + ((1-alpha)*adc_filtrado);
+   Serial1.print(adc_raw);
+   Serial1.print(",");  
+   Serial1.println(adc_filtrado);*/
 }
