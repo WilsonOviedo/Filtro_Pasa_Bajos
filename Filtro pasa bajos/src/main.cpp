144e9b06592e3347b851ff1fd33ca5f209aa6d33
Willy <wilsonoh@outlook.es>
2019-06-19 13:13:53 -0400

    Codigo basico listo

--- Filtro pasa bajos/src/main.cpp
+++ Filtro pasa bajos/src/main.cpp
@@ -1,24 +1,35 @@
 #include <Arduino.h>
+/*
+Este ejemplo se probo en un microcontrolador STM32F103C8T6 (BlackPill)
+Caso desea compilar para otra plataforma cambie las configuraciones en Platformio.ini
+Y tambie cambie el pin Analogo que desea leer de acuerdo a su placa 
+Si lo compila para una placa arduino cualquiera cambie Serial3 por Serial, para que pueda visualizar
+correctamente los datos.
+ */
+/*
+Filtro Media Móvil como Pasa Bajos
+An=a*M+(1-a)*An
+alpha 1: Sin filtro
+alpha 0: Filtrado totalmente
+alpha mas comun 0.05
+*/
 
-//Filtro Media Móvil como Pasa Bajos
-//An=a*M+(1-a)*An
-//alpha 1: Sin filtro
-//alpha 0: Filtrado totalmente
-//alpha clásico 0.05
 
 int adc_filtrado = 0;
 int adc_raw = 0;
-#define alpha 0.05
+#define alpha 0.05   //Alpha
+#define pin PA7      //Pin de lectura
 
 void setup(){
-   Serial3.begin(4800);
-   Serial3.print("Holaaa"); 
+   Serial3.begin(4800);   
+   pinMode(pin,INPUT_ANALOG);
+   delay(500);   
 }
 
 void loop(){
-  /*  adc_raw = analogRead(PA0);
+   adc_raw = analogRead(pin);
    adc_filtrado = (alpha*adc_raw) + ((1-alpha)*adc_filtrado);
-   Serial1.print(adc_raw);
-   Serial1.print(",");  
-   Serial1.println(adc_filtrado);*/
+   Serial3.print(adc_raw);
+   Serial3.print(",");  
+   Serial3.println(adc_filtrado);
 }
